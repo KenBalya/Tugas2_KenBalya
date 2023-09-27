d554cf0b461a1d4e04cd0b4f94e92683f3a0cc46
KenBalya <124903836+KenBalya@users.noreply.github.com>
2023-09-27 11:59:33 +0700

    Tugas 3

--- movie_list/main/models.py
+++ movie_list/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Movie(models.Model):
     movie_name = models.CharField(max_length=255)
@@ -6,3 +7,4 @@ class Movie(models.Model):
     rating = models.IntegerField()#Rating dari film
     likes = models.IntegerField(default=0)#Jumlah likes dari film(equivalent dengan amount pada soal)
     description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
